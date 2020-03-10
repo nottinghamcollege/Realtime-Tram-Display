31dc4ebd08c8b77ce5175b7fb758e10e31aef38b
James White <james@jmwhite.co.uk>
2020-03-10 13:55:34 +0000

    Update tram.py to return timetable if not real time

--- tram.py
+++ tram.py
@@ -7,10 +7,8 @@ from datetime import datetime
 
 parser = argparse.ArgumentParser()
 parser.add_argument('atco_code', help='A valid ATCO Code e.g. 9400ZZNOHIG2')
+parser.add_argument('limit', help='The amount of times to return e.g. 4', nargs='?', type=int, default=5)
 args = parser.parse_args()
-
-if args.atco_code is None:
-    sys.exit('Please provide an ATCO Code')
 
 def print_info(text):
     print(colour.Fore.WHITE + text)
@@ -26,22 +24,26 @@ tram_data = requests.get('https://robinhood.arcticapi.com/network/stops/' + args
 if '_links' in tram_data:
     stop_name = tram_data['_links']['naptan:stop']['commonName']
     indicator = tram_data['_links']['naptan:stop']['indicator']
-    
+
     print_info(stop_name + ' ' + indicator)
     print_info(datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
     print(' ')
 
     if '_embedded' in tram_data:
         visits = tram_data['_embedded']['timetable:visit']
-        for visit in visits:
-             if visit['isRealTime']:
-                     print_tram('Destination: ' + visit['destinationName'])
-                     if visit['expectedArrivalTime'] == visit['aimedArrivalTime']:
-                            print_good('On time!')
-                     else:
-                            print_bad('Running late!')
+        for visit in visits[:args.limit]:
+            print_tram('Destination: ' + visit['destinationName'])
+            
+            if visit['isRealTime']:
+                if visit['expectedArrivalTime'] == visit['aimedArrivalTime']:
+                    print_good('On time!')
+                else:
+                    print_bad('Running late!')
 
-                     print_info('Expected time of arrival: ' + visit['displayTime'])
-                     print(' ')
+            else:
+                print_info('Note: Arrival time displayed is not in real time.')
+                                
+            print_info('Expected time of arrival: ' + visit['displayTime'])
+            print(' ') 
 else:
     sys.exit('No stop data was returned, are you sure this is a valid ATCO Code?')
